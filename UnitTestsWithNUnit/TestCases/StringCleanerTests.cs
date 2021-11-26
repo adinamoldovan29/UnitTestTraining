6b2a1bca32318a06aa6ff904cdbc82d570aae18a
Adina Moldovan <adina.moldovan@amdaris.com>
2021-11-26 14:21:59 +0200

    Change one line

--- UnitTestsWithNUnit/TestCases/StringCleanerTests.cs
+++ UnitTestsWithNUnit/TestCases/StringCleanerTests.cs
@@ -7,7 +7,7 @@ namespace UnitTest.TestCases
     [TestFixture]
     public class StringCleanerTests
     {
-        //[TestCase("", "")]
+        [TestCase("123", "123")]
         [TestCase("test with space", "testwithspace")]
         [TestCase("testwithoutspace", "testwithoutspace")]
         public void Clean_TextWithSpaces_SpacesRemoved(string text, string expectedResult)
